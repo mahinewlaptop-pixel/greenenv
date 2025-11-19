7e253f55229c84d51e6a24cc6c233ad3ffc05161
Mahi Kumar <mahinewlaptop@gmail.com>
2025-11-19 08:45:01 +0530

    navbar oversized issue fixed

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 export default function Navbar() {
   const [like, setLike]=useState(0);
   return (
-    <nav>
+    <nav className="bg-[#4C763B] flex justify-between items-center h-12 mb-12 max-w-screen">
       
-      <div className="bg-[#4C763B] flex justify-between items-center h-12 mb-12 w-screen">
-        <div className="px-8 ">
+      
+        <div className="ps-4 ">
             <a className="navText font-bold " href="">GREENenv.</a>
         </div>
         <div>
-          <i onClick={()=> setLike(like+1)} className=" cursor-pointer text-4xl fa-solid fa-heart text-white mx-8 active:text-red-600 hover:animate-pulse active:animate-ping "><span className="text-[#4C763B] relative right-7 bottom-2.5 text-xs">{like}</span></i>
+          <i onClick={()=> setLike(like+1)} className=" cursor-pointer text-4xl fa-solid fa-heart text-white ms-4 active:text-red-600 hover:animate-pulse active:animate-ping "><span className="text-[#4C763B] relative right-7 bottom-2.5 text-xs">{like}</span></i>
         </div>
         
         <div className="flex justify-evenly flex-wrap">
@@ -18,9 +18,6 @@ export default function Navbar() {
             <a className="navText" href="https://my-resume-builder-livid.vercel.app/">More</a>
             <a className="navText" href="">Contact</a>
         </div>
-        
-        
-      </div>
     </nav>
   );
 }
